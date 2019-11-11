01aad62e76cbda40edc497ec406a0f8d933c85a9
Grant Houston <grant.houston44@hotmail.com>
2019-11-11 20:27:44 +0000

    made attempt at extension before battery died with no charger

---
+++ library/library.rb
@@ -0,0 +1,12 @@
+class Library
+
+  def initialize(input_books)
+    @books = input_books
+  end
+
+  def get_books
+    @books
+  end
+
+
+end
--- library/specs/library_specs.rb
+++ library/specs/library_specs.rb
@@ -1,5 +1,37 @@
 require('minitest/autorun')
 require('minitest/rg')
-require_relative('..library.rb')
+require_relative('../library.rb')
 
 class TestLibrary < MiniTest::Test
+
+  def setup
+    @library = Library.new(
+      {
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
+        }
+      }
+    )
+  end
+
+  def test_create_library_object
+    library = Library.new(books)
+  end
+
+  def test_get_books
+      #don't need to create new instance of library object as library object already created in def setup
+      #call the getter and assert the books eequal to the library Hash
+      assert_equal({
+        title: "lord_of_the_rings",
+        rental_details: {
+          student_name: "Jeff",
+          date: "01/12/16"
+        }
+      }, library.get_books)
+  end
+
+
+
+end

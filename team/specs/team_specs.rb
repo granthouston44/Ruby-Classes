4bdb2c584ffb530be0b2b315a8a1bc48ad61f791
Grant Houston <grant.houston44@hotmail.com>
2019-11-11 16:57:24 +0000

    completed team getters

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -4,12 +4,32 @@ require_relative('../team')
 
 class TestTeam < MiniTest::Test
 
-def test_creat_team_object
+def test_create_team_object
   team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
 end
 
+#create test for getters
+def test_get_team_name
+  #create new instance of the team class
+  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+  #call the getter and assert that the name is equal to code clan Rovers
+  assert_equal("Code Clan Rovers", team.team_name)
+end
+
+def test_get_players
+  #create new instance of team class
+  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+  #call the players getter and asser that the players array is assert_equal
+  assert_equal(["Anna", "Ally", "Colin", "Lindsay"], team.players)
+end
+
+def test_get_coach
+  #create new instance of team class
+  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+  #call the getter method for coach and assert that its equal to Maria
+  assert_equal("Maria", team.coach)
 
 
-
+end
 
 end
--- team/team.rb
+++ team/team.rb
@@ -1,11 +1,21 @@
 class Team
 
-  def initialize(team_name, players, coach)
-    @team_name = name
-    @players = players
-    @coach = coach
+  def initialize(input_team_name, input_players, input_coach)
+    @team_name = input_team_name
+    @players = input_players
+    @coach = input_coach
   end
 
+  def team_name
+    @team_name
+  end
 
+  def players
+    @players
+  end
+
+  def coach
+    @coach
+  end
 
 end

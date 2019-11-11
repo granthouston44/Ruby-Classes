d5b8304e128af6fab983d71cc7f29cb523fe8b84
Grant Houston <grant.houston44@hotmail.com>
2019-11-11 17:23:40 +0000

    add player method

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -4,32 +4,51 @@ require_relative('../team')
 
 class TestTeam < MiniTest::Test
 
-def test_create_team_object
-  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
-end
+  def test_create_team_object
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+  end
 
-#create test for getters
-def test_get_team_name
-  #create new instance of the team class
-  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
-  #call the getter and assert that the name is equal to code clan Rovers
-  assert_equal("Code Clan Rovers", team.team_name)
-end
+  #create test for getters
+  def test_get_team_name
+    #create new instance of the team class
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+    #call the getter and assert that the name is equal to code clan Rovers
+    assert_equal("Code Clan Rovers", team.team_name)
+  end
 
-def test_get_players
-  #create new instance of team class
-  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
-  #call the players getter and asser that the players array is assert_equal
-  assert_equal(["Anna", "Ally", "Colin", "Lindsay"], team.players)
-end
+  def test_get_players
+    #create new instance of team class
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+    #call the players getter and asser that the players array is assert_equal
+    assert_equal(["Anna", "Ally", "Colin", "Lindsay"], team.players)
+  end
 
-def test_get_coach
-  #create new instance of team class
-  team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
-  #call the getter method for coach and assert that its equal to Maria
-  assert_equal("Maria", team.coach)
+  def test_get_coach
+    #create new instance of team class
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+    #call the getter method for coach and assert that its equal to Maria
+    assert_equal("Maria", team.coach)
+  end
 
+  def test_set_coach_name
+    #create new instance of team class
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+    #call the setter method and change coach name to Niall
+    team.coach = "Niall"
+    #assert that the name has been changed from Maria to Niall
+    assert_equal("Niall", team.coach)
+  end
 
-end
+  def test_add_new_player
+    #create new instance of team class
+    team = Team.new("Code Clan Rovers", ["Anna", "Ally", "Colin", "Lindsay"], "Maria")
+    #call the add new player method and add Andrew
+    team.add_new_player("Andrew")
+    #assert that the players array has increased in size
+    assert_equal(5, team.players.length)
+    # p team.players
+
+  end
+
 
 end
--- team/team.rb
+++ team/team.rb
@@ -1,21 +1,33 @@
 class Team
 
+  attr_accessor :team_name, :players, :coach
+
   def initialize(input_team_name, input_players, input_coach)
     @team_name = input_team_name
     @players = input_players
     @coach = input_coach
   end
 
-  def team_name
-    @team_name
+  #longform defining of setters and getters
+  #   def team_name
+  #     @team_name
+  #   end
+  #
+  #   def players
+  #     @players
+  #   end
+  #
+  #   def coach
+  #     @coach
+  #   end
+  #
+  # def set_coach(coach_name)
+  # @coach = coach_name
+  # end
+
+  def add_new_player(new_player)
+    @players.push(new_player)
   end
 
-  def players
-    @players
-  end
 
-  def coach
-    @coach
   end
-
-end

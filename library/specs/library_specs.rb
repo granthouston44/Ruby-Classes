5218b6a2614a93e9f861f14c90d54e845e43153d
Grant Houston <grant.houston44@hotmail.com>
2019-11-11 20:29:59 +0000

    getter passed

--- library/specs/library_specs.rb
+++ library/specs/library_specs.rb
@@ -16,10 +16,6 @@ class TestLibrary < MiniTest::Test
     )
   end
 
-  def test_create_library_object
-    library = Library.new(books)
-  end
-
   def test_get_books
       #don't need to create new instance of library object as library object already created in def setup
       #call the getter and assert the books eequal to the library Hash
@@ -29,7 +25,7 @@ class TestLibrary < MiniTest::Test
           student_name: "Jeff",
           date: "01/12/16"
         }
-      }, library.get_books)
+      }, @library.get_books)
   end
 
 

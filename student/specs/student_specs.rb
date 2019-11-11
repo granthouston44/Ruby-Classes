df80853720083f9fc27c62b2e2a2bed5603f72f6
Grant Houston <grant.houston44@hotmail.com>
2019-11-11 15:44:33 +0000

    tested both getter methods - passed

--- student/specs/student_specs.rb
+++ student/specs/student_specs.rb
@@ -4,14 +4,28 @@ require_relative('../student')
 
 class TestStudent < MiniTest::Test
 
+#test that class has been created and we can create a student object
   def test_can_create_student_object
+#create new student object and pass in parameters
     student = Student.new("Grant", "G16")
   end
 
+#test student name method
+  def test_student_name
+    #create new student instance named Grant
+    grant = Student.new("Grant", "G16")
+    #use getter method to grab student object name
+    #assert that student object has name grant
+    assert_equal("Grant", grant.name)
+  end
 
-
-
-
-
+#test student cohort method
+def test_student_cohort
+  #create new student instance
+  student = Student.new("Grant", "G16")
+  #use getter method to grab student cohort number
+  #assert that the student object instance has cohort G16
+  assert_equal("G16", student.cohort)
+end
 
 end
--- student/student.rb
+++ student/student.rb
@@ -1,5 +1,7 @@
 class Student
 
+attr_accessor :name, :cohort
+
 def initialize(input_name, input_cohort)
 @name = input_name
 @cohort = input_cohort
